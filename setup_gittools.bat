b50b9ca4b3f4cd07d6d7e2656fbac5585705c1e9
Diederick C. Niehorster <dcnieho@gmail.com>
2012-08-26 07:53:14 +0800

    setup_gittools.bat can now also do hooks when parent is a submodule

--- setup_gittools.bat
+++ setup_gittools.bat
@@ -1,15 +1,26 @@
 @echo off
+
+
 IF EXIST ..\.git\hooks (
     xcopy pre-commit ..\.git\hooks /Y
-) ELSE (
-    :: note, if ..\.git is a file, it contains the link to where the real .git folder can be found. so <that link>\hooks is where we should copy to in that case
-    echo !!you need to copy pre-commit by hand to .git/modules/^<module name^>/hooks
+    goto :setupDeps
+) ELSE IF EXIST ..\.git (
+    SET /p gitLink=<..\.git
 )
 
+IF "%gitLink%" EQU "" (
+    echo !! cannot resolve location to put pre-commit hook, do it manually
+    goto:setupDeps
+)
+:: build path to copy to
+SET gitLink=../%gitLink:~8%/hooks
+SET gitLink=%gitLink:/=\%
+xcopy pre-commit %gitLink% /Y
+
+
+:setupDeps
 :: now cd to parent and add a git command to run script that sets all dependencies to expected version
-
 cd ..
-
 git config alias.getdeps "!python git-tools/checkoutDeps.py"
 
 echo setup done, use 'git getdeps' to set all deps to expected state
